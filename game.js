95cb571d883557cd037720e6fe068de4ad91f071
Maureen <220000@student.glu.nl>
2023-06-23 10:44:23 +0200

    last commit

--- game.js
+++ game.js
@@ -5,26 +5,19 @@ function Game(canvas) {
   this.width = canvas.width
   this.height = canvas.height
 
-  // Keep track of key states
-  // Eg.:
-  //   game.keyPressed.up === true  // while UP key is pressed)
-  //   game.keyPressed.up === false // when UP key is released)
+
   this.keyPressed = {}
 
   $(canvas).on('keydown keyup', function(e) {
-    // Convert key code to key name
     var keyName = Game.keys[e.which]
 
     if (keyName) {
-      // eg.: `self.keyPressed.up = true` on keydown
-      // Will be set to `false` on keyup
       self.keyPressed[keyName] = e.type === 'keydown'
       e.preventDefault()
     }
   })
 }
 
-// Some key code to key name mappings
 Game.keys = {
   32: 'space',
   37: 'left',
@@ -47,24 +40,8 @@ Game.prototype.draw = function() {
   })
 }
 
-// A simple (but unreliable) game loop.
-//
-// The problem with this approach is that the update and draw operations
-// are glued together. If the timer is not able to run at the proper interval
-// (if CPU is too busy for example), the game will appear sluggish.
-//
-//   Game.prototype.start = function() {
-//     var self = this,
-//         fps = 60,
-//         interval = 1000 / fps // ms per frame
-//   
-//     setInterval(function() {
-//       self.update()
-//       self.draw()
-//     }, interval)
-//   }
 
-// Here is a real game loop. Similar to the ones you'll find in most games.
+
 Game.prototype.start = function() {
   var self = this
 
